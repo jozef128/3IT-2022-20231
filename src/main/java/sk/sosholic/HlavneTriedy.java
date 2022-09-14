94c34a59fcf8efc783517bc01768fa0e841738b8
jozef pradlovsky <pradlovskyjozef70@gmail.com>
2022-09-14 11:11:21 +0200

    Initial commit

---
+++ src/main/java/sk/sosholic/HlavneTriedy.java
@@ -0,0 +1,14 @@
+package sk.sosholic;
+
+public class HlavneTriedy {
+    public static void main (String[] args){
+
+        System.out.println("Int uchovava prirodzene cislo velkosti 32b");
+        System.out.println("Boolean uvhovava prirodzene cislo velkosti 8b");
+        System.out.println("Char uchovava prirodzene cislo velkosti 16b");
+        System.out.println("Long uchovava prirodzene cislo velkosti 64b");
+        System.out.println("Float uchovava prirodzene cislo velkosti 32b");
+        System.out.println("Double uchovava prirodzene cislo velkosti 64b]" );
+
+    }
+}
---
+++ src/main/java/sk/sosholic/Main.java
@@ -0,0 +1,15 @@
+package sk.sosholic;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+
+            System.out.println("Int uchovava prirodzene cislo velkosti 32b");
+            System.out.println("Boolean uvhovava prirodzene cislo velkosti 8b");
+            System.out.println("Char uchovava prirodzene cislo velkosti 16b");
+            System.out.println("Long uchovava prirodzene cislo velkosti 64b");
+            System.out.println("Float uchovava prirodzene cislo velkosti 32b");
+            System.out.println("Double uchovava prirodzene cislo velkosti 64b]" );
+        }
+    }
+
